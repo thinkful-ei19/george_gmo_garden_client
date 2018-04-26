bb6de98049c2edc678781b3dc2f3c8d79b41f08a
George Colon <geocolon@gmail.com>
2018-04-26 11:09:42 -0700

    Updating actions with AJAX

--- src/actions/index.js
+++ src/actions/index.js
@@ -11,6 +11,19 @@
 //     }
 // };
 
+export const fetchPlayers = () => dispatch => {
+    fetch('${API_BASE_URL}/game/players').then(res => {
+        if (!res.ok) {
+            return Promise.reject(res.statusText);
+        }
+        return res.json();
+    }).then(player => {
+        dispatch(fetchPlayersSuccess(player));
+    }).catch(err => {
+        dispatch(fetchPlayerError(err));
+    });    
+};
+
 export const CHANGE_PLAYER_SCORE = 'CHANGE_PLAYER_SCORE'; 
 export const changePlayerScore = score => ({ 
     type: CHANGE_PLAYER_SCORE,
